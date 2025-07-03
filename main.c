2ab38f949ef09d62021f1a8fb1a91e0137694fd7
cncptpr <concept1928@gmail.com>
2025-07-03 19:39:50 +0200

    Init

---
+++ main.c
@@ -0,0 +1,122 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// #define DBG_DIAG_INDEXS
+// #define DBG_COORD_INFO
+
+int xy_to_ddown(int x, int y, int n) { return (y - x) + n - 1; }
+int xy_to_dup(int x, int y, int n) { return (n - y - x) + n - 2; }
+
+typedef struct {
+  int n, *x, *y, *dup, *ddown;
+} Field;
+
+int is_zero(Field field, int x, int y) {
+  return !field.x[x] && !field.y[y] && !field.dup[xy_to_dup(x, y, field.n)] &&
+         !field.ddown[xy_to_ddown(x, y, field.n)];
+}
+void assign(Field field, int x, int y, int value) {
+  field.x[x] = field.y[y] = field.dup[xy_to_dup(x, y, field.n)] =
+      field.ddown[xy_to_ddown(x, y, field.n)] = value;
+}
+
+int solve(int queen, int x0, Field field) {
+#ifdef DBG
+  printf("%d\n", queen);
+  for (int x = 0; x < field.n; x++) {
+    for (int y = 0; y < field.n; y++) {
+      if (field.x[x] != 0 && field.x[x] == field.y[y])
+        printf("%2d ",field.x[x]);
+      else printf(" 0 ");
+    }
+    printf("\n");
+  }
+  printf("\n");
+#endif // DBG
+  if (field.n == queen-1)
+    return 1;
+  for (int x = x0; x < field.n; x++) {
+    if (field.x[x])
+      continue;
+    for (int y = 0; y < field.n; y++) {
+      if (is_zero(field, x, y)) {
+        assign(field, x, y, queen);
+        if(solve(queen+1, x, field)) {
+          return 1;
+        }
+        assign(field, x, y, 0);
+      }
+    }
+  }
+  return 0;
+}
+
+#ifdef DBG_DIAG_INDEXS
+int main(int argc, char **argv) {
+  if (argc < 2) {
+    printf("usage: n-queens <n>\n");
+    return 0;
+  }
+  int n = atoi(argv[1]);
+  if (!n) {
+    printf("Error: %s is not a valide value vor n\n", argv[1]);
+    printf("usage: n-queens <n>\n");
+    return 1;
+  }
+  for (int x = 0; x < n; x++) {
+    for (int y = 0; y < n; y++)
+      printf("(%d, %d):%2d ", x, y, xy_to_ddown(x, y, n));
+    printf("\n");
+  }
+  printf("\n");
+  printf("\n");
+  for (int x = 0; x < n; x++) {
+    for (int y = 0; y < n; y++)
+      printf("(%d, %d):%2d ", x, y, xy_to_dup(x, y, n));
+    printf("\n");
+  }
+}
+#else
+int main(int argc, char **argv) {
+  if (argc < 2) {
+    printf("usage: n-queens <n>\n");
+    return 0;
+  }
+  int n = atoi(argv[1]);
+  if (!n) {
+    printf("Error: %s is not a valide value vor n\n", argv[1]);
+    printf("usage: n-queens <n>\n");
+    return 1;
+  }
+  Field field = {
+      .n = n,
+      .x = calloc(n, sizeof(int)),
+      .y = calloc(n, sizeof(int)),
+      .dup = calloc(2 * n - 1, sizeof(int)),
+      .ddown = calloc(2 * n - 1, sizeof(int)),
+  };
+
+  assert(solve(1, 0, field));
+
+  for (int x = 0; x < n; x++) {
+    for (int y = 0; y < n; y++) {
+      if (field.x[x] != 0 && field.x[x] == field.y[y])
+#ifdef DBG_COORD_INFO
+        printf("(%2d, %2d, %2d, %2d) q", field.x[x], field.y[y],
+               field.dup[xy_to_dup(x, y, n)],
+               field.ddown[xy_to_ddown(x, y, n)]);
+      else
+        printf("(%2d, %2d, %2d, %2d) .", field.x[x], field.y[y],
+               field.dup[xy_to_dup(x, y, n)],
+               field.ddown[xy_to_ddown(x, y, n)]);
+#else
+        printf("q ");
+      else
+        printf(". ");
+#endif
+    }
+    printf("\n");
+  }
+}
+#endif
